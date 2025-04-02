f74456e02d14a412c80105785237154d31e67cd0
Julia Collingwood <jcollingwood001@student.franciscan.edu>
2025-04-02 17:30:07 -0400

    added light mode button

--- lightMode.js
+++ lightMode.js
@@ -2,3 +2,22 @@ function toggleLightMode() {
     document.documentElement.classList.toggle("light-mode");
 }
 
+/* 
+
+window.addEventListener("DOMContentLoaded", () => {
+    const chosenTheme = localStorage.getItem("theme"); 
+
+    if (chosenTheme === "light") {
+        document.body.classLast.add("light-mode");
+        }
+    else {
+        document.body.classList.remove("light-mode");
+    }
+});
+
+function toggleLightMode() {
+    const isLightMode = document.body.classList.contains("light-mode");
+    localStorage.setItem("theme", isLightMode ? "light" : "dark");
+}
+
+*/
